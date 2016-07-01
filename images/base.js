24f4bad7db96f5a51f7d7ca78c6d5fb03f97902c
wanyongzhen <1041282946@qq.com>
2016-07-01 17:08:02 +0800

    no message

---
+++ images/base.js
@@ -0,0 +1,32 @@
+
+var isShowAd = true; // 是否显示广告 总开关
+var isShowIndexAd = true; // 是否显示首页上的广告
+var isShowTopicAd = true; // 是否显示topic文章广告
+var isShowCommandAd = true; // 是否显示命令广告
+
+$.getUrlParam = function (name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+};
+
+
+
+/**
+ * 判断是否是移动浏览器
+ */
+function isMobileBrowser(){
+		var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+        return true;
+    }
+    return false;
+}
